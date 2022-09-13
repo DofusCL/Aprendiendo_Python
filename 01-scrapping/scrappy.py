c1946dd27420b025e6fb21e32c83ce4955f29f94
Nicolás Bosoni Spinetto <nicobosoni@gmail.com>
2022-09-13 17:56:22 +0200

    Update scrappy.py

--- 01-scrapping/scrappy.py
+++ 01-scrapping/scrappy.py
@@ -1,9 +1,9 @@
 import tweepy
 
-consumer_key = '46qg1HnLazDbKJhri9SG2Nuwz'
-consumer_secret ='8AhAF2no3pVUdL87C3jEmGKoX14qG0fBNzNfW8oxqhfM5qPJPd' 
-access_token = '1007255574944378882-0n1qOB52KDouC6hoEKtYhtNOKzjQpN'
-access_token_secret = 'fPhk1p4Z4Sj50sXwZBg6Q8wWEJOVfVdV7ArLiFWKjHEoE'
+consumer_key = ''
+consumer_secret ='' 
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
